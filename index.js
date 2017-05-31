24bda578162e090f82a8616dedd718d4e2b61aa2
thinkzhan <thinkzhan@sohu-inc.com>
2017-05-31 22:30:38 +0800

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+require('shelljs/global');
+var console = require('sfconsole')("vtl-kit", true );
+
+module.exports = {
+    init: function(opts) {
+        console.info('clone seed....');
+        exec(`git clone https://github.com/liberalist1991/webpack-velocity-tpl.git ${opts.to}`);
+        console.info('install dependencies....');
+        console.warn('please wating...');
+        exec(`cd ${opts.to||'webpack-velocity-tpl'} && npm install`);
+        console.info(`init ${opts.to||'webpack-velocity-tpl'} ok!`);
+
+    }
+}
