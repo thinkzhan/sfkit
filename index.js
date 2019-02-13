5eaba8aa9c4073c2b6ec36411b818d373979f4dd
thinkzhan <thinkzhan@sohu-inc.com>
2019-02-13 15:36:37 +0800

    upd

--- index.js
+++ index.js
@@ -1,36 +1,50 @@
-require('shelljs/global');
-const console = require('sfconsole')("vtl-kit", true);
-const inquirer = require('inquirer')
-const tplMap = require('./map')
+require("shelljs/global");
+const console = require("sfconsole")("sf-kit", true);
+const inquirer = require("inquirer");
+const tplMap = require("./map");
 
 module.exports = {
-    template: '',
-    projectName: '',
+    template: "",
+    projectName: "",
 
     init() {
-        this.askTemplate().then(answers => {
-            this.template = answers.template;
-            return this.askName()
-        }).then(answers => {
-            this.projectName = answers.projectName || this.template;
-            console.info('init seed....');
-            exec(
-                `git clone ${tplMap[this.template]['repo']} ${this.projectName}`
-            );
-            console.warn('install dependencies...  several minutes');
-            exec(`cd ${this.projectName} && npm install`);
-            console.warn(
-                `init ${this.projectName} ok! cd ${this.projectName} && npm run dev`
-            );
-        });
+        this.askTemplate()
+            .then(answers => {
+                this.template = answers.template;
+                return this.askName();
+            })
+            .then(answers => {
+                this.projectName = answers.projectName || this.template;
+                console.info("init seed....");
+                console.info(
+                    `clone ${tplMap[this.template]["branch"]} ==> ${
+                        this.projectName
+                    }`
+                );
+                exec(
+                    `git clone -b ${tplMap[this.template]["branch"]} ${
+                        tplMap[this.template]["repo"]
+                    } ${this.projectName}`
+                );
+
+                console.warn("install dependencies...  several minutes");
+                exec(`cd ${this.projectName} && npm install`);
+                console.warn(
+                    `init ${this.projectName} ok! cd ${
+                        this.projectName
+                    } && npm run dev`
+                );
+            });
     },
 
     askName() {
-        return inquirer.prompt([{
-            type: 'input',
-            name: 'projectName',
-            message: '输入项目名称：'
-        }])
+        return inquirer.prompt([
+            {
+                type: "input",
+                name: "projectName",
+                message: "输入项目名称："
+            }
+        ]);
     },
 
     askTemplate() {
@@ -40,15 +54,17 @@ module.exports = {
             choices.push({
                 key: index,
                 name: tplMap[key].desc,
-                value: key,
-            })
+                value: key
+            });
         });
 
-        return inquirer.prompt([{
-            type: 'list',
-            name: 'template',
-            message: '以下模版可选：',
-            choices: choices
-        }]);
+        return inquirer.prompt([
+            {
+                type: "list",
+                name: "template",
+                message: "以下模版可选：",
+                choices: choices
+            }
+        ]);
     }
-}
+};
--- map.js
+++ map.js
@@ -1,31 +1,31 @@
 module.exports = {
-    'single-js':  {
-        'desc' : 'single-js                   一个单文件js',
-        'repo' : 'https://github.com/liberalist1991/fe-template.git',
-        'branch': 'single-js'
+    "single-js": {
+        desc: "single-js                   一个单文件js",
+        repo: "https://github.com/liberalist1991/fe-template.git",
+        branch: "single-js"
     },
 
-    'velocity-mutil-page':  {
-        'desc' : 'velocity-mutil-page         一个多页项目',
-        'repo' : 'https://github.com/liberalist1991/fe-template.git',
-        'branch': 'velocity-mutil-page'
+    "velocity-mutil-page": {
+        desc: "velocity-mutil-page         一个velocity多页项目",
+        repo: "https://github.com/liberalist1991/fe-template.git",
+        branch: "velocity-mutil-page"
     },
 
-    'velocity-mutil-page-h5':  {
-        'desc' : 'velocity-mutil-page-h5      一个多页h5项目',
-        'repo' : 'https://github.com/liberalist1991/fe-template.git',
-        'branch': 'velocity-mutil-page-h5'
-    },
-    
-    'mdev-mutil-page':  {
-        'desc' : 'mdev-mutil-page             一个兼容mdev的多页项目',
-        'repo' : 'https://github.com/liberalist1991/fe-template.git',
-        'branch': 'mdev-mutil-page'
+    "velocity-mutil-page-h5": {
+        desc: "velocity-mutil-page-h5      一个velocity多页h5项目",
+        repo: "https://github.com/liberalist1991/fe-template.git",
+        branch: "velocity-mutil-page-h5"
     },
 
-    'mdev-mutil-page-h5':  {
-        'desc' : 'mdev-mutil-page-h5          一个兼容mdev的多页h5项目',
-        'repo' : 'https://github.com/liberalist1991/fe-template.git',
-        'branch': 'mdev-mutil-page-h5'
+    "vue-mutil-page": {
+        desc: "vue-mutil-page              一个vue多页项目",
+        repo: "https://github.com/liberalist1991/fe-template.git",
+        branch: "vue-mutil-page"
+    },
+
+    "vue-admin": {
+        desc: "vue-admin                   一个vue后台项目",
+        repo: "https://github.com/liberalist1991/fe-template.git",
+        branch: "vue-admin"
     }
-}
+};
